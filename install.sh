9598102cc0ec756612d2e85ce16b88c0b2118a21
Даниил Пронин <mail@grawl.ru>
2020-01-26 19:51:07 +1000

    fix password test

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # load environment variables from .env
 export $(egrep -v '^#' .env | xargs)
@@ -6,7 +6,7 @@ export $(egrep -v '^#' .env | xargs)
 PASSWORD_MIN_LENGTH=8
 
 # check password length
-if [[ ${#MEDIAWIKI_ADMIN_PASSWORD} -lt ${PASSWORD_MIN_LENGTH} ]]; \
+if [ ${#MEDIAWIKI_ADMIN_PASSWORD} -lt ${PASSWORD_MIN_LENGTH} ]; \
 	then echo "Password must be $PASSWORD_MIN_LENGTH or more symbols"; exit 1; \
 	fi
 
